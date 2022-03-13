88d049391cbee1c41bc974d7673cfa2aab7d4c47
Thomas <thomasrobertsbradford@gmail.com>
2022-03-13 17:35:20 -0500

    controllers,routes,middleware are done 3/13/22

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,31 +1,37 @@
+const asyncHandler = require('express-async-handler')
+
 
 // @desc     Get goals
 // @route    GET /api/goals
 // @access   Private
-const getGoals = (req,res) => {
+const getGoals =  asyncHandler(async (req,res) => {
     res.status(200).json({message:'get goals'}) // .status to show custom status on retrieval
-}
+})
 
 // @desc     Post goal
 // @route    POST /api/goals
 // @access   Private
-const setGoal = (req,res) => {
+const setGoal = asyncHandler( async (req,res) => {
+    if (!req.body.text) { //if not
+        res.status(400)
+        throw new Error('Please add text field') // specifies error
+    }
     res.status(201).json({message:'Set goal'}) 
-}
+})
 
 // @desc     Update a goal
 // @route    PUT /api/goal/:id
 // @access   Private
-const putGoal = (req,res) => {
+const putGoal = asyncHandler (async (req,res) => {
     res.status(200).json({message:`Update goal ${req.params.id}`}) // access params
-}
+})
 
 // @desc     Delete a goal
 // @route    DELETE /api/goals/:id
 // @access   Private
-const deleteGoal = (req,res) => {
+const deleteGoal = asyncHandler (async (req,res) => {
     res.status(200).json({message:`Delete goal ${req.params.id}`}) // access params
-}
+})
 
 module.exports = {
     getGoals,
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const dotenv = require('dotenv').config() // config allows dotenv file
 const port = process.env.port || 5000 // to access env
-
+const {errorHandler} = require('./middleware/errorMiddleware')
 const app = express()
 
+app.use(express.json()) // body parser for json data NEEDED for POST req body data
+app.use(express.urlencoded({extended:false}))
+
 app.use('/api/goals',require('./routes/goalRoutes'))
+
+app.use(errorHandler) // overrides default error handler
 
 app.listen(port,() => console.log(`server started on ${port}`))
